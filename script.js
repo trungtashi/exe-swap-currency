cc0070ea10faf1cfca09c6e94515598939acc7a4
trungtashi99 <trungtashi@gmai.com>
2022-07-08 11:25:26 +0700

    Update bài tập chuyển đổi tiền tệ

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function convert(){
     let inputCurrency = document.getElementById('currency').value
         fromCurrency = document.getElementById('from-currency').value
         toCurrency = document.getElementById('to-currency').value
-        toCurrencyName = document.getElementById('to-currency').name
 
         rate = fromCurrency/toCurrency
         result = inputCurrency/rate
